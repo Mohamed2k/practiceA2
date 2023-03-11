2c26f614078757dd07d8d08adc7c0e4ae5114a22
Mohamed Issa <80771950+Mohamed2k@users.noreply.github.com>
2023-03-11 12:19:31 -0500

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,19 @@
+"""
+Functions to test
+"""
+
+def add(first_num, second_num):
+    """
+    This function adds two inputs together
+    :param first_num: param add :param second_num: param
+    :return: sum
+    """
+    return first_num + second_num
+
+def reverse(string_or_list):
+    """
+    This function adds two inputs together
+    :param string_or_list: param
+    :return: reverse
+    """
+    return string_or_list[::-1]
